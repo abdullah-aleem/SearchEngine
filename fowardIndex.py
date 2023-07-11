ce1504cbe5de03535d0911b0afa15bd72791a119
abdullah-aleem <62750535+abdullah-aleem@users.noreply.github.com>
2023-07-11 23:30:01 +0500

    writingLexAndFor_toFile

--- fowardIndex.py
+++ fowardIndex.py
@@ -2,6 +2,10 @@ import json
 from cleaner import cleaning_text
 import pandas as pd
 import time
+import os
+
+# Path to the JSON file
+file_path = "forwardIndex.json"
 
 
 
@@ -15,6 +19,26 @@ def forwardIndex(file):
             data=pd.Series(data)
             x=data.apply(cleaning_text,args=(words,))
             print(x[0])
-    return words
+    return x
 
-forwardIndex("newsdata/newsbud.json")
+app=forwardIndex("newsdata/newsbud.json")
+
+
+
+
+# Check if the file exists
+if os.path.exists(file_path):
+    # File exists, open it and load the existing data
+    with open(file_path, "r") as file:
+        jdata = json.load(file)
+else:
+    # File doesn't exist, create an empty data dictionary
+    jdata = []
+
+
+# Save the updated data to the JSON file
+jdata+=list(app)
+
+with open(file_path, "w+") as file:
+   
+    json.dump(jdata, file)
--- lexicon.py
+++ lexicon.py
@@ -4,7 +4,10 @@ import pandas as pd
 import time
 
 
+import os
 
+# Path to the JSON file
+file_path = "lexicon.json"
 
 def lexicon(file):
     words = []
@@ -17,6 +20,7 @@ def lexicon(file):
   
         data=pd.Series(data)
         data.apply(cleaning_text,args=(words,))
+    #now to include in the file for lexicons
     return words
 
 
@@ -27,3 +31,20 @@ execution_time = end_time - start_time
 
 # print("Lexicon:", result)
 print("Execution Time:", execution_time, "seconds")
+
+# Check if the file exists
+if os.path.exists(file_path):
+    # File exists, open it and load the existing data
+    with open(file_path, "r") as file:
+        jdata = json.load(file)
+else:
+    # File doesn't exist, create an empty data dictionary
+    jdata = []
+
+
+# Save the updated data to the JSON file
+jdata+=list(result)
+
+with open(file_path, "w+") as file:
+   
+    json.dump(jdata, file)
