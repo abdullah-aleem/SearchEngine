7a7464ebe49cb67cdc53dfca97f53fbfd1f00889
abdullah-aleem <62750535+abdullah-aleem@users.noreply.github.com>
2023-07-13 22:06:24 +0500

    InvertedIndex_writing_to_file

--- invertedIndex.py
+++ invertedIndex.py
@@ -9,10 +9,12 @@ for word in words:
     temp=[]
 
     for doc in data:
-        title=list(data[0].keys())[0]
-        content=list(data[0].values())[0]
+        title=list(doc.keys())[0]
+        content=list(doc.values())[0]
+        
         if word in content:
-            if title in temp:
-                continue
             temp.append(title)
     invData.append({word:temp}) 
+    
+with open("invertedIndex.json", "w+") as file:   
+    json.dump(invData, file)
