c8a67b97d1195781df01785ab8fa270ee48fb39d
abdullah-aleem <62750535+abdullah-aleem@users.noreply.github.com>
2023-07-17 15:25:13 +0500

    CompletedOnAllDirectory
    lexicon and forward Index are now working on all directory using multiprocessing to create different pool of each file

--- fowardIndex.py
+++ fowardIndex.py
@@ -3,13 +3,15 @@ from cleaner import cleaning_text
 import pandas as pd
 import time
 import os
-
+import multiprocessing
 # Path to the JSON file
 file_path = "forwardIndex.json"
-
-
+directory="newsdata/"
+app=[]
 
 def forwardIndex(file):
+    global app
+    print("hello this is forward index")
     words=[]
     with open(file, "r") as file:
             # Load the JSON data
@@ -18,26 +20,44 @@ def forwardIndex(file):
         if data:
             data=pd.Series(data)
             x=data.apply(cleaning_text,args=(words,))
+            x=list(x)
+            app+=x
     return x
 
-app=forwardIndex("newsdata/cnbc.json")
-
-
-
-# Check if the file exists
-if os.path.exists(file_path):
-    # File exists, open it and load the existing data
-    with open(file_path, "r") as file:
-        jdata = json.load(file)
-else:
-    # File doesn't exist, create an empty data dictionary
-    jdata = {}
-# Save the updated data to the JSON file
-
-
-
-for y in list(app):
+# app=forwardIndex("newsdata/cnbc.json")
+if __name__=='__main__':
     
-    jdata.update(y)
-with open(file_path, "w+") as file:
-    json.dump(jdata, file)
+    # Get the list of files in the directory
+    file_list = os.listdir(directory)
+
+    # Create a multiprocessing Pool with the number of desired processes
+    pool = multiprocessing.Pool()
+
+    # Map the process_file function to each file in the file_list
+    # This will start a new process for each file
+    pool.map(forwardIndex, [os.path.join(directory, file) for file in file_list])
+
+    # Close the pool to prevent any more processes from being started
+    pool.close()
+
+    # Wait for all the processes to finish
+    pool.join()
+
+
+    # Check if the file exists
+    if os.path.exists(file_path):
+        # File exists, open it and load the existing data
+        with open(file_path, "r") as file:
+            jdata = json.load(file)
+    else:
+        # File doesn't exist, create an empty data dictionary
+        jdata = {}
+    # Save the updated data to the JSON file
+
+
+
+    for y in list(app):
+        
+        jdata.update(y)
+    with open(file_path, "w+") as file:
+        json.dump(jdata, file)
--- lexicon.py
+++ lexicon.py
@@ -3,12 +3,12 @@ from cleaner import cleaning_text
 import pandas as pd
 import time
 import os
-
-
+import multiprocessing
 # Path to the JSON file
 file_path = "lexicon.json"
 
-
+directory="newsdata/"
+result=[]
 def lexicon(file):
     words = []
     
@@ -21,23 +21,39 @@ def lexicon(file):
         data=pd.Series(data)
         data.apply(cleaning_text,args=(words,))
     #now to include in the file for lexicons
+    result+=words
     return words
 
 
 
+if __name__=='__main__':
+      # Get the list of files in the directory
+    file_list = os.listdir(directory)
 
-result = lexicon("newsdata/cnbc.json")
-# Check if the file exists
-if os.path.exists(file_path):
-    # File exists, open it and load the existing data
-    with open(file_path, "r") as file:
-        jdata = json.load(file)
-else:
-    # File doesn't exist, create an empty data dictionary
-    jdata = []
-# Save the updated data to the JSON file
-jdata+=list(result)
-jdata=set(jdata)
-jdata=list(jdata)
-with open(file_path, "w+") as file:   
-    json.dump(jdata, file)
+    # Create a multiprocessing Pool with the number of desired processes
+    pool = multiprocessing.Pool()
+
+    # Map the process_file function to each file in the file_list
+    # This will start a new process for each file
+    pool.map(lexicon, [os.path.join(directory, file) for file in file_list])
+
+    # Close the pool to prevent any more processes from being started
+    pool.close()
+
+    # Wait for all the processes to finish
+    pool.join()
+   
+    # Check if the file exists
+    if os.path.exists(file_path):
+        # File exists, open it and load the existing data
+        with open(file_path, "r") as file:
+            jdata = json.load(file)
+    else:
+        # File doesn't exist, create an empty data dictionary
+        jdata = []
+    # Save the updated data to the JSON file
+    jdata+=list(result)
+    jdata=set(jdata)
+    jdata=list(jdata)
+    with open(file_path, "w+") as file:   
+        json.dump(jdata, file)
